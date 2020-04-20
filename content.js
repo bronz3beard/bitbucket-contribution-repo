ad5c725d1d8b4b1f5048229c0abf27c88983a885
rory <rory@thefleetoffice.com.su>
2020-04-20 03:33:02 +0000

    add code in files types: .php,.js remove code in files types: .php,.js

--- content.js
+++ content.js
@@ -1,2 +1,2 @@
 console.log("wwtld")
-console.log("sbfkf")
+console.log("yhtml")
--- content.php
+++ content.php
@@ -2,5 +2,5 @@
         echo "bqkum";
         echo "ovjmy";
         echo "yooix";
-        echo "qncth";
+        echo "psupu";
 ?>
