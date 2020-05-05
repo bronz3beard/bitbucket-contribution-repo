a752e842e9e0957816c6c90c6932014406dc3d86
rory <rory@thefleetoffice.com.au>
2020-05-05 07:05:30 +0000

    add code in files types: .php,.js remove code in files types: .php,.js

--- content.js
+++ content.js
@@ -1,3 +1,3 @@
 console.log("yxdoa")
 console.log("gyeim")
-console.log("rwwyy")
+console.log("kfqvm")
--- content.php
+++ content.php
@@ -5,5 +5,5 @@
         echo "grcnb";
         echo "jtqpn";
         echo "bvuhr";
-        echo "hmyot";
+        echo "nuaak";
 ?>
